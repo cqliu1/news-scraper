3f81502c7500a0abfc54542dd67662fb4d4e3b1e
Catherine Liu <catherineqliu@outlook.com>
2017-08-26 16:16:26 -0700

    Fixed port on server

--- server.js
+++ server.js
@@ -139,7 +139,7 @@ app.post("/article/:id", function(req, res) {
 });
 
 
-// Listen on port 3000
-app.listen(3000, function() {
+// Listen on port
+app.listen(port, function() {
   console.log("App running on port 3000!");
 });
